de81a5d81ac973d10b8eed32c40e24e04077cb28
Junyan Ren <ren.ju@northeastern.edu>
2022-05-25 12:59:32 -0400

    update router and product details

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,19 +1,22 @@
 import { Container } from "react-bootstrap";
+import {BrowserRouter as Router, Route} from 'react-router-dom'
 import Footer from "./components/Footer";
 import Header from "./components/Header";
 import Home from "./views/Home";
+import Products from "./views/Products";
 
 function App() {
   return (
-    <div>
+    <Router>
       <Header/>
       <main className="py-3">
         <Container>
-          <Home/>
+          <Route path='/' component={Home} exact/>
+          <Route path='/products/:id' component={Products}/>
         </Container>
       </main>
       <Footer/>
-    </div>
+    </Router>
   );
 }
 
