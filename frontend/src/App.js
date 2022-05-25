bbb36c49fc690d7cfc67caaae546f9e370bbffd5
Junyan Ren <ren.ju@northeastern.edu>
2022-05-24 20:28:37 -0400

    update header & footer

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,7 +1,17 @@
+import { Container } from "react-bootstrap";
+import Footer from "./components/Footer";
+import Header from "./components/Header";
+
 function App() {
   return (
     <div>
-      app
+      <Header/>
+      <main className="py-3">
+        <Container>
+          <h1>hello</h1>
+        </Container>
+      </main>
+      <Footer/>
     </div>
   );
 }
